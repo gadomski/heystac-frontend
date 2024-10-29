2e0b2558d9e7e13df0966ab599f493e1bf89dfa5
Pete Gadomski <pete.gadomski@gmail.com>
2024-10-29 05:05:37 -0600

    fix: call the root catalog Root

--- app/catalogs/[id]/page.tsx
+++ app/catalogs/[id]/page.tsx
@@ -1,14 +1,14 @@
 import CatalogPage from "../../components/pages/catalog";
-import Catalog from "../../catalog.json";
+import Root from "../../catalog.json";
 import { use } from "react";
 import { notFound } from "next/navigation";
-import type { Catalog as Stac } from "../../types/Stac";
+import type { Catalog } from "../../types/Stac";
 
 type Params = { id: string };
 
 export default function Page({ params }) {
   const { id } = use(params) as Params;
-  const link = Catalog["links"].find(link => link["heystac:id"] == id);
+  const link = Root["links"].find((link) => link["heystac:id"] == id);
   if (!link) {
     notFound();
   }
@@ -16,8 +16,8 @@ export default function Page({ params }) {
 }
 
 export async function generateStaticParams() {
-  const stac_catalog: Stac = Catalog;
-  return stac_catalog.links.map(link => ({
+  const stac_catalog: Catalog = Root;
+  return stac_catalog.links.map((link) => ({
     id: link["heystac:id"],
   }));
 }
