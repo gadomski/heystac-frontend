ae717acafcb059e6d48be6010e48918fa9faeeae
Pete Gadomski <pete.gadomski@gmail.com>
2024-11-08 10:24:26 -0700

    feat: crawl, and use those files (#25)

--- src/heystac/__init__.py
+++ src/heystac/__init__.py
@@ -5,14 +5,24 @@ from .config import Config
 
 @click.group()
 def cli() -> None:
+    """Command-line interface for heystac"""
     pass
 
 
 @click.command()
 def prebuild() -> None:
+    """Build our catalog"""
     Config().prebuild()
 
 
+@click.command()
+@click.argument("id")
+def crawl(id: str) -> None:
+    """Crawl a STAC API"""
+    Config().crawl(id)
+
+
+cli.add_command(crawl)
 cli.add_command(prebuild)
 
 
