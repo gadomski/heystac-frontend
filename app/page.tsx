8a85723e1aedf0411d44a1fa214faa00bea2d64d
Tammo Feldmann <tammo.feldmann@gmail.com>
2024-11-11 12:01:41 -0500

    Update page to using chakra

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,23 @@
 
 import CatalogsCard from "./components/card/catalog";
 import Catalog from "./catalog.json";
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Grid } from "@chakra-ui/react";
 
 export default function Home() {
   // TODO: Use Catalog type
-  let catalogs = Catalog["links"].filter(link => link["rel"] == "child");
+  let catalogs = Catalog["links"].filter(link => link["rel"] === "child");
+
   return (
-    <div className="h-full grid grid-cols-1 text-center content-center">
-      <div className="my-8">
-        <Heading className="py-2" size="xl">
-          heystac
-        </Heading>
-
-        <Text>A curated geospatial asset discovery experience™</Text>
-      </div>
-
-      <CatalogsCard catalogs={catalogs}></CatalogsCard>
-    </div>
+    <Box h="full" textAlign="center">
+      <Grid h="100%" templateColumns="1fr" placeContent="center">
+        <Box my="8">
+          <Heading py="2" size="xl">
+            heystac
+          </Heading>
+          <Text>A curated geospatial asset discovery experience™</Text>
+        </Box>
+        <CatalogsCard catalogs={catalogs} />
+      </Grid>
+    </Box>
   );
 }
